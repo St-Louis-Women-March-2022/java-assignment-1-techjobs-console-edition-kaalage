7d858420d353d3d5a856ac03150112debb4473f9
Karina Lage <karinar_lage@live.com>
2022-08-17 21:52:27 -0500

    Tried fixing printJobs() to pass test

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -94,11 +94,14 @@ public class JobData {
      * @return      List of all jobs with at least one field containing the value
      */
     public static ArrayList<HashMap<String, String>> findByValue(String value) {
-
         // load data, if not already loaded
         loadData();
 
         // TODO - implement this method
+
+
+
+
         return null;
     }
 
